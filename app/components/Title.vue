2b9a0a7693b17e5774654575330f9a16d407cc6d
Morgan Kpassi <>
2026-02-27 15:18:55 +0100

    feat: home page
    feat: modal
    
    feat: budget section
    
    feat: depense by category swiper

---
+++ app/components/Title.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    const props = defineProps<{
+        title: string
+    }>()
+</script>
+
+<template>
+    <h2 class="text-lg lg:text-2xl font-bold mb-2">{{ title }}</h2>
+</template>
---
+++ app/components/TitlePage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    const props = defineProps<{
+        title: string,
+        description: string
+    }>()
+</script>
+<template>
+    <section class="mb-4">
+        <h1 class="text-lg lg:text-3xl font-bold mb-2">{{ props.title }}</h1>
+        <p class="text-gray-600 text-sm lg:text-md">{{ props.description }}</p>
+    </section>
+</template>
